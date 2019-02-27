0de10f23e112ce9caf1cc2f75303d604a721aaeb
Stu Freen <stuart.freen@rangle.io>
2019-02-27 09:41:38 -0500

    Update android icons and splash screen

--- android/app/src/main/java/com/breeze/MainActivity.java
+++ android/app/src/main/java/com/breeze/MainActivity.java
@@ -1,6 +1,14 @@
 package com.breeze;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
