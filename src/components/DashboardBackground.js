6ed5d635ce3f9a43f9a25a2665a62bc7bf8c1664
Stu Freen <stuart.freen@rangle.io>
2019-02-28 15:07:54 -0500

    Add additional background images

--- src/components/DashboardBackground.js
+++ src/components/DashboardBackground.js
@@ -3,50 +3,96 @@ import { Animated } from 'react-native';
 import PropTypes from 'prop-types';
 import ThemedImage from './ThemedImage';
 
-const clearImageWine = require('../assets/backgrounds/clear-wine.jpg');
-const rainImageWine = require('../assets/backgrounds/rain-wine.jpg');
-const snowImageWine = require('../assets/backgrounds/snow-wine.jpg');
-const cloudyImageWine = require('../assets/backgrounds/cloudy-wine.jpg');
+const clearDayWine = require('../assets/backgrounds/wine/clear-day-wine.jpg');
+const clearNightWine = require('../assets/backgrounds/wine/clear-night-wine.jpg');
+const partlyCloudyDayWine = require('../assets/backgrounds/wine/partly-cloudy-day-wine.jpg');
+const partlyCloudyNightWine = require('../assets/backgrounds/wine/partly-cloudy-night-wine.jpg');
+const rainWine = require('../assets/backgrounds/wine/rain-wine.jpg');
+const snowWine = require('../assets/backgrounds/wine/snow-wine.jpg');
+const cloudyWine = require('../assets/backgrounds/wine/cloudy-wine.jpg');
+const fogWine = require('../assets/backgrounds/wine/fog-wine.jpg');
+const windWine = require('../assets/backgrounds/wine/wind-wine.jpg');
 
-const clearImageElectric = require('../assets/backgrounds/clear-electric.jpg');
-const rainImageElectric = require('../assets/backgrounds/rain-electric.jpg');
-const snowImageElectric = require('../assets/backgrounds/snow-electric.jpg');
-const cloudyImageElectric = require('../assets/backgrounds/cloudy-electric.jpg');
+const clearDayElectric = require('../assets/backgrounds/electric/clear-day-electric.jpg');
+const clearNightElectric = require('../assets/backgrounds/electric/clear-night-electric.jpg');
+const partlyCloudyDayElectric = require('../assets/backgrounds/electric/partly-cloudy-day-electric.jpg');
+const partlyCloudyNightElectric = require('../assets/backgrounds/electric/partly-cloudy-night-electric.jpg');
+const rainElectric = require('../assets/backgrounds/electric/rain-electric.jpg');
+const snowElectric = require('../assets/backgrounds/electric/snow-electric.jpg');
+const cloudyElectric = require('../assets/backgrounds/electric/cloudy-electric.jpg');
+const fogElectric = require('../assets/backgrounds/electric/fog-electric.jpg');
+const windElectric = require('../assets/backgrounds/electric/wind-electric.jpg');
 
 const getImage = (iconKey) => {
   switch (iconKey) {
     case 'clear-day':
+      return (
+        <ThemedImage
+          sources={{ default: clearDayWine, electric: clearDayElectric }}
+          resizeMode="cover"
+        />
+      );
     case 'clear-night':
+      return (
+        <ThemedImage
+          sources={{ default: clearNightWine, electric: clearNightElectric }}
+          resizeMode="cover"
+        />
+      );
     case 'wind':
       return (
         <ThemedImage
-          sources={{ electric: clearImageElectric, default: clearImageWine }}
+          sources={{ default: windWine, electric: windElectric }}
           resizeMode="cover"
         />
       );
     case 'rain':
-    case 'sleet':
       return (
         <ThemedImage
-          sources={{ electric: rainImageElectric, default: rainImageWine }}
+          sources={{ default: rainWine, electric: rainElectric }}
           resizeMode="cover"
         />
       );
+    case 'sleet':
     case 'snow':
       return (
         <ThemedImage
-          sources={{ electric: snowImageElectric, default: snowImageWine }}
+          sources={{ default: snowWine, electric: snowElectric }}
           resizeMode="cover"
         />
       );
     case 'fog':
+      return (
+        <ThemedImage
+          sources={{ default: fogWine, electric: fogElectric }}
+          resizeMode="cover"
+        />
+      );
     case 'cloudy':
+      return (
+        <ThemedImage
+          sources={{ default: cloudyWine, electric: cloudyElectric }}
+          resizeMode="cover"
+        />
+      );
     case 'partly-cloudy-day':
+      return (
+        <ThemedImage
+          sources={{ default: partlyCloudyDayWine, electric: partlyCloudyDayElectric }}
+          resizeMode="cover"
+        />
+      );
     case 'partly-cloudy-night':
+      return (
+        <ThemedImage
+          sources={{ default: partlyCloudyNightWine, elecric: partlyCloudyNightElectric }}
+          resizeMode="cover"
+        />
+      );
     default:
       return (
         <ThemedImage
-          sources={{ electric: cloudyImageElectric, default: cloudyImageWine }}
+          sources={{ default: cloudyWine, electric: cloudyElectric }}
           resizeMode="cover"
         />
       );
