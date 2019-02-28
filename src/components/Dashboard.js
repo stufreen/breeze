3ef305ebcb74cea61e5d2c2a90a720324974f3c7
Stu Freen <stuart.freen@rangle.io>
2019-02-28 13:58:59 -0500

    Dashboard hero fade out (#1)
    * Fade out hero with animated
    
    * Ditch the animated for fading out the hero
    
    * Add POP to hourly chart

---
+++ src/components/Dashboard.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, ScrollBox } from './design-system';
+import DashboardHero from './DashboardHero';
+import Hourly from './Hourly';
+import SettingsButton from './SettingsButton';
+import LongTerm from './LongTerm';
+import Alert from './Alert';
+import DashboardBackground from './DashboardBackground';
+import bp from '../services/breakpoints';
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollY: 0,
+    };
+  }
+
+  handleScroll = (e) => {
+    this.setState({
+      scrollY: e.nativeEvent.contentOffset.y,
+    });
+  }
+
+  render() {
+    const { weather, onPressSettings } = this.props;
+    const { scrollY } = this.state;
+    return (
+      <Box flex={1} position="relative">
+        <DashboardBackground iconKey={weather.currently.icon} />
+        <Box position="absolute" width="100%" pt={4}>
+          <DashboardHero scrollY={scrollY} />
+        </Box>
+        <ScrollBox
+          flex={1}
+          contentContainerStyle={{ minHeight: '100%', paddingTop: bp([400, 500]) }}
+          scrollEventThrottle={16}
+          onScroll={this.handleScroll}
+          onMomentumScrollEnd={this.handleScroll}
+        >
+          <Box>
+            <Alert />
+            <Hourly />
+            <LongTerm />
+          </Box>
+        </ScrollBox>
+        <Box position="absolute" mt={4} ml={3}>
+          <SettingsButton
+            onPress={onPressSettings}
+          />
+        </Box>
+      </Box>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  onPressSettings: PropTypes.func.isRequired,
+  weather: PropTypes.shape({ currently: PropTypes.object }).isRequired,
+};
+
+export default Dashboard;
