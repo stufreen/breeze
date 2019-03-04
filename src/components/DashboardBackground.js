a6828dc66a4d13152874c1aa69808079cbb0de6c
Stu Freen <stuart.freen@rangle.io>
2019-03-03 19:11:11 -0500

    Fix permission message; Fix white line on background; Refresh weather on restart

--- src/components/DashboardBackground.js
+++ src/components/DashboardBackground.js
@@ -78,7 +78,7 @@ class DashboardBackground extends React.Component {
             top: 0,
             left: -1,
             right: 0,
-            bottom: 0,
+            bottom: -1,
             resizeMode: 'cover',
             opacity: bgOpacity,
           }}
