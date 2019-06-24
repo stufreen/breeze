e5b67b85d3ab57c0f265023af5429ec10bb1a9d4
Stu Freen <stuart.freen@rangle.io>
2019-06-24 17:29:58 -0400

    Fix carousel bug when orientation changes

--- src/components/DashboardCarousel.js
+++ src/components/DashboardCarousel.js
@@ -4,33 +4,58 @@ import PropTypes from 'prop-types';
 import Carousel from 'react-native-snap-carousel';
 import DashboardSlide from './DashboardSlide';
 
-const DashboardCarousel = ({
-  locations,
-  currentSlide,
-  setCurrentSlide,
-  refreshWeather,
-}) => {
-  const { width } = Dimensions.get('screen');
-  return (
-    <Carousel
-      index={currentSlide}
-      onSnapToItem={(activePageIndex) => {
-        setCurrentSlide(activePageIndex);
-        refreshWeather(activePageIndex);
-      }}
-      activeAnimationType="spring"
-      data={locations}
-      renderItem={({ item }) => (
-        <DashboardSlide
-          location={item}
-        />
-      )}
-      sliderWidth={width}
-      itemWidth={width}
-      inactiveSlideScale={1}
-    />
-  );
-};
+class DashboardCarousel extends React.Component {
+  constructor() {
+    super();
+    const { width } = Dimensions.get('screen');
+    this.state = {
+      slideWidth: width,
+    };
+  }
+
+  componentDidMount() {
+    Dimensions.addEventListener('change', this.updateWidth);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.updateWidth);
+  }
+
+  updateWidth = ({ screen }) => {
+    this.setState({
+      slideWidth: screen.width,
+    });
+  }
+
+  render() {
+    const {
+      locations,
+      currentSlide,
+      setCurrentSlide,
+      refreshWeather,
+    } = this.props;
+    const { slideWidth } = this.state;
+    return (
+      <Carousel
+        index={currentSlide}
+        onSnapToItem={(activePageIndex) => {
+          setCurrentSlide(activePageIndex);
+          refreshWeather(activePageIndex);
+        }}
+        activeAnimationType="spring"
+        data={locations}
+        renderItem={({ item }) => (
+          <DashboardSlide
+            location={item}
+          />
+        )}
+        sliderWidth={slideWidth}
+        itemWidth={slideWidth}
+        inactiveSlideScale={1}
+      />
+    );
+  }
+}
 
 DashboardCarousel.defaultProps = {
   locations: [],
